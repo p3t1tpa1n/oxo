e3d3ad8bdf1c3bbb4b6b37da7eab7b8db3637120
p3t1tpa1n <paul.pfl03@gmail.com>
2025-04-03 10:10:41 +0200

    Fix: Resolve serviceWorkerVersion undefined error and improve browser compatibility checks

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,3 +1,5 @@
 // This script handles the old bootstrapping method
 // It will be replaced by Flutter's build process
-var serviceWorkerVersion = null; 
+if (typeof serviceWorkerVersion === 'undefined') {
+  var serviceWorkerVersion = null;
+} 
